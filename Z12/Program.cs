db79eb5e8f2580c7ad1bbe43ed0044367b4aba30
Alexey <alekseynovikov65@gmail.com>
2022-12-21 17:10:26 +0300

    Add max1

---
+++ Z12/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите 1-e число");
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2-e число");
+int numB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 3-e число");
+int numC = int.Parse(Console.ReadLine());
+//int max = 0;
+if (numA > numB)
+{
+    if (numA < numC)
+    {
+        int max = numC;
+        Console.WriteLine($"Число {max} - больше всех");
+    }
+    else{
+        int max = numA;
+        Console.WriteLine($"Число {max} - больше всех");
+    }
+
+}
+if (numB < numC)
+{
+    int max = numC;
+    Console.WriteLine($"Число {max} - больше всех");
+}
+else{
+    int max = numB;
+    Console.WriteLine($"Число {max} - больше всех");
+}
---
+++ Z13/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Введите чило: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine($"Число {num} четное.");
+}
+else{
+    Console.WriteLine($"Число {num} нечетное. ");
+}
