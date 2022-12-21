f14bd52001327e0e8bbae1932343546bc8ec1d1c
Alexey <alekseynovikov65@gmail.com>
2022-12-21 13:13:54 +0300

    Add Max and Min

---
+++ Z11/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");  //
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");  //
+int numB = int.Parse(Console.ReadLine());
+//int max = 0;
+if (numA > numB)
+{
+    System.Console.WriteLine($"Число {numA} больше {numB}");
+}
+else{
+    System.Console.WriteLine($"Число {numB} больше {numA}");
+}
