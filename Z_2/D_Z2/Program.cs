3efa3b7fa0c83cf85effe88ddc78e2ecba5dd483
Alexey <alekseynovikov65@gmail.com>
2022-12-21 17:50:42 +0300

    Add Z_2

---
+++ Z_2/D_Z2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите число: от 100 до 888 ");
+string A = (Console.ReadLine());
+int num = int.Parse(A);
+if (num > 100 && num <= 999)
+{
+    char x = A[1];
+    Console.WriteLine($"Число - {x} ");
+}
+else{
+    System.Console.WriteLine($"Читай условие задачи!");
+}
