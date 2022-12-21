f9c117f9f3e7a8e25a5b9572cd3dfe975ad68ff4
Alexey <alekseynovikov65@gmail.com>
2022-12-21 17:19:19 +0300

    Add zad14

---
+++ Z14/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число - ");
+int N = int.Parse(Console.ReadLine());
+int i = 2;
+int count = 0;
+while (i <= N)
+{
+    int x = i % 2;
+    if (x == 0)
+    {
+        Console.WriteLine($" {i}, ");
+    }
+i++;
+}
+
+
+
+
